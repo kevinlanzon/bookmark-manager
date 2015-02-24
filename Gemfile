34519811ea1731e0ca09a72a70b2296106ef5195
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-24 17:45:56 +0000

    adding sinatra and BookmarkManager class. Tests passing

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 source "https://rubygems.org"
 
+gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'database_cleaner'
+gem 'capybara'
 
 group :test do
   gem 'rspec'
--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
 require 'data_mapper'
 
+class BookmarkManager < Sinatra::Base
+
+  get '/' do
+    @links = Link.all
+    erb :index
+  end
+
+end
+
+
 env = ENV['RACK_ENV'] || 'development'
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
