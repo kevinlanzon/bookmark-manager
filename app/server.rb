a7a0fab036d474edc3868eb89ad065e83ae7a6be
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-24 15:54:28 +0000

    changed tree structure and renamed files

---
+++ app/server.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require 'data_mapper'
+
+env = ENV['RACK_ENV'] || 'development'
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './app/models/link'
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
